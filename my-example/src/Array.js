58574d2875f95467a81a7d6a2896d873d71465f1
LeVHoangduc <levhoangduc@gmail.com>
2023-10-20 11:21:05 +0700

    feat:implement example for array

---
+++ my-example/src/Array.js
@@ -0,0 +1,19 @@
+import AddArray from "./components/ArrayExample/AddArray";
+import InsertArray from "./components/ArrayExample/InsertArray";
+import RemoveArray from "./components/ArrayExample/RemoveArray";
+import ReplaceItem from "./components/ArrayExample/ReplaceItem";
+import TransformArray from "./components/ArrayExample/TransformArray";
+import UpdateObjectInArray from "./components/ArrayExample/UpdateObjectinArray";
+
+export default function Array() {
+  return (
+    <>
+      <AddArray />
+      <InsertArray />
+      <RemoveArray />
+      <ReplaceItem />
+      <TransformArray />
+      <UpdateObjectInArray />
+    </>
+  );
+}
--- my-example/src/index.js
+++ my-example/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Array from "./Array";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Example for thinking in React */}
+    {/* <App /> */}
+
+    {/* Example for array in state */}
+    <Array />
   </React.StrictMode>
 );
 
