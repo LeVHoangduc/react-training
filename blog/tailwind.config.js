8da012053cec0056141c6f0f02ce9aa6f150ef00
LeVHoangduc <levhoangduc@gmail.com>
2023-11-06 09:29:32 +0700

    feat:update tailwindconfig

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'custom-white': '#ffffff',
+        'custom-black': '#121212',
+        'custom-gray': '#9d9d9d',
+        'custom-blue': '#268bd2',
+        'custom-light': '#eeeeee', // --strong-color
+        'custom-dark-gray': '#202020', //--sidebar-bg-color
+        'custom-medium-gray': '#909090', //     --sidebar-p-color
+        'custom-light-gray': '#9a9a9a', // --date-color
+        'custom-light-white': '#DBE2E9' // --title-color-dark-theme
+      },
+      fontFamily: {
+        fira: ['fira sans', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
