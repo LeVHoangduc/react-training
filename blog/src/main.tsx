1f5b979221b77281133b5349f91d809da7045b63
LeVHoangduc <levhoangduc@gmail.com>
2023-11-23 11:29:16 +0700

    feat:fix react router with netlify

--- blog/src/main.tsx
+++ blog/src/main.tsx
@@ -6,12 +6,12 @@ import App from './App.tsx'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
