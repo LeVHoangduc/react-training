30280ff5238e7671bb853d92174edf88c6defe4f
LeVHoangduc <levhoangduc@gmail.com>
2023-11-23 10:57:27 +0700

    feat:fix deploy error for module json

--- blog/src/declarations.d.ts
+++ blog/src/declarations.d.ts
@@ -2,3 +2,8 @@ declare module '*.md' {
   const content: string
   export default content
 }
+
+declare module '../posts/Jsons/posts.json' {
+  const content: Json // Replace YourType with the actual type of your JSON content
+  export default content
+}
