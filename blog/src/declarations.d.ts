bba97cbf6325c69ea5e6e388bc5f0a701d2f7a58
LeVHoangduc <levhoangduc@gmail.com>
2023-11-23 11:14:53 +0700

    feat:update deploy

--- blog/src/declarations.d.ts
+++ blog/src/declarations.d.ts
@@ -2,8 +2,3 @@ declare module '*.md' {
   const content: string
   export default content
 }
-
-declare module '../src/Jsons/posts.json' {
-  const postList: Json // Replace YourType with the actual type of your JSON content
-  export default postList
-}
