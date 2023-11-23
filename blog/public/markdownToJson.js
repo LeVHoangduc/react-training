fd6b89343ea814bedb08f3f5158298f37fa2dc26
LeVHoangduc <levhoangduc@gmail.com>
2023-11-23 10:29:03 +0700

    feat:update field of post

--- blog/public/markdownToJson.js
+++ blog/public/markdownToJson.js
@@ -2,9 +2,7 @@ function markdownToJSON(markdown) {
   const data = {
     title: '',
     author: '',
-    link: '',
     date: '',
-    image: '',
     tags: [],
     content: ''
   }
@@ -22,12 +20,8 @@ function markdownToJSON(markdown) {
       data.title = getValueFromLine(trimmedLine)
     } else if (trimmedLine.startsWith('author:')) {
       data.author = getValueFromLine(trimmedLine)
-    } else if (trimmedLine.startsWith('link:')) {
-      data.link = getValueFromLine(trimmedLine)
     } else if (trimmedLine.startsWith('date:')) {
       data.date = getValueFromLine(trimmedLine)
-    } else if (trimmedLine.startsWith('image:')) {
-      data.image = getValueFromLine(trimmedLine)
     } else if (trimmedLine.startsWith('tags:')) {
       data.tags = getArrayValueFromLine(trimmedLine)
     } else {
