5f9f39124ed916f8fba04a4d27ae50200061955d
LeVHoangduc <levhoangduc@gmail.com>
2023-11-27 10:39:14 +0700

    feat:update readme

--- blog/src/main.tsx
+++ blog/src/main.tsx
@@ -1,12 +1,10 @@
-import './wdyr'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 
 import './index.css'
 
 import App from './App.tsx'
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
